9cd2a8a58f1a5bc6ffbc22bb9df46674731333a2
huy-vu-mulodo <nguyenhuyvu@Hiens-iMac.local>
2014-09-09 15:21:39 +0700

    ban account

--- fuel/app/classes/controller/LoginUser.php
+++ fuel/app/classes/controller/LoginUser.php
@@ -37,14 +37,25 @@ class Controller_LoginUser extends Controller_Rest
 	            );
 	            return;
 	        }
-	        
+	        //Add Date: 09/09/2014 - Name :Tran Quoc Dung Start
+	        if($data_user['user_ban'] == 1)
+	        {
+	        	$this->Response(
+	        			 array(
+	                            'status'    => 408,
+	                            'message'   => "User was banned",
+	                    )
+	        	);
+	        	return;
+	        }
+	        //Add Date : 09/09/2014 - Name: Tran Quoc Dung End
 	        
 	    } catch (Exception $e) {
 	        
 	        $this->Response(
 	                array(
 	                        'status'    => 500,
-	                        'message'   => "InternalÊServerÊError",
+	                        'message'   => "InternalÃŠServerÃŠError",
 	                )
 	        );
 	        return;
@@ -64,6 +75,7 @@ class Controller_LoginUser extends Controller_Rest
 	            array(
 	                    'status'  => 200,
 	                    'message' => "Login Successfull",
+	                    'token' => $token_id;
 	            )
 	    );
 	    
